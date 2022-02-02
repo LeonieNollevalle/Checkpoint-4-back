549d77a59774f644d733f6d5a215a32e5257a3ef
LeonieNollevalle <leonie.nollevalle@gmail.com>
2022-02-02 16:59:17 +0100

    add route get categorie

--- models/categorie.js
+++ models/categorie.js
@@ -1,8 +1,14 @@
 const connection = require('../db-config');
 
 const db = connection.promise();
+const findMany = () => {
+  let sqlQuery = 'SELECT * FROM categorie';
+  return db.query(sqlQuery)
+    .then((result) => result[0]);
+};
 
 
 
 module.exports ={
+  findMany
 };
